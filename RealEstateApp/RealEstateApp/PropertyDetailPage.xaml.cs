31f8cb6361bf93358944ec291bd8dcae4ee767e2
Mike Mortensen <mike2137@elevcampus.dk>
2022-08-23 12:57:02 +0200

    Implemented TTS feature

--- RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
+++ RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using RealEstateApp.Models;
 using RealEstateApp.Services;
 using System.Linq;
+using System.Threading;
 using TinyIoC;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -26,9 +28,39 @@ namespace RealEstateApp
 
         public Property Property { get; set; }
 
+        private bool _isPlaying;
+        public bool IsPlaying
+        {
+            get => _isPlaying;
+            set
+            {
+                if (_isPlaying != value)
+                {
+                    _isPlaying = value;
+                    OnPropertyChanged(nameof(_isPlaying));
+                }
+            }
+        }
+
+        private CancellationTokenSource cSource = new CancellationTokenSource();
+
         private async void EditProperty_Clicked(object sender, System.EventArgs e)
         {
             await Navigation.PushAsync(new AddEditPropertyPage(Property));
+        }
+
+        private async void StartTTS(object sender, System.EventArgs e)
+        {
+            IsPlaying = true;
+            await TextToSpeech.SpeakAsync(Property.Description, cSource.Token);
+            IsPlaying = false;
+        }
+
+        private void StopTTS(object sender, System.EventArgs e)
+        {
+            cSource.Cancel();
+            IsPlaying = false;
+            cSource = new CancellationTokenSource();
         }
     }
 }
